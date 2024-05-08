aaa318a57d66fb9e4fb24ef85873205afdb0200b
duytnmse <duytnmse173241@fpt.edu.vn>
2024-05-08 14:49:46 +0700

    update ReactDom18

--- react-webpack/src/index.js
+++ react-webpack/src/index.js
@@ -1,14 +1,22 @@
 import React from "react"; // nạp thư viện react
-import ReactDOM from "react-dom"; // nạp thư viện react-dom
+import ReactDOM from "react-dom/client"; // nạp thư viện react-dom
 
 // Tạo component App
 function App() {
   return (
-    <div>
-      <h1>Xin chào anh em F8!</h1>
-    </div>
+    <>
+      <div>
+        <h1>Xin chào anh em F8!</h1>
+      </div>
+      <p>Hello ae</p>
+    </>
   );
 }
 
 // Render component App vào #root element
-ReactDOM.render(<App />, document.getElementById("root"));
+//@React17
+//ReactDOM.render(<App />, document.getElementById("root"));
+
+//@React18
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<App />);
